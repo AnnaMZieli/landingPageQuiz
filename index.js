045cfdb4f2ac2de330937697315108e2f9b9f002
AnnaMZieli <anna.zielinska@app4ad.pl>
2024-07-29 16:45:24 +0200

    all images added to slide show, prepared to desktop and mobile

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -14,34 +12,20 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let slidesToShow = window.innerWidth > 600 ? 3 : 1;
+
+  if (n > slides.length - slidesToShow + 1) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length - slidesToShow + 1}
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+
+  for (i = 0; i < slidesToShow; i++) {
+    slides[(slideIndex - 1 + i) % slides.length].style.display = "flex";
+  }
 }
 
-// let slideIndex = 0;
-// const slidesToShow = window.innerWidth > 600 ? 3 : 1;
-// const slides = document.getElementsByClassName('mySlides');
-// const totalSlides = slides.length;
-
-// function showSlides(n) {
-//     const slideWrapper = document.querySelector('.slideshow-wrapper');
-//     slideIndex = (n + totalSlides) % totalSlides;
-//     slideWrapper.style.transform = `translateX(-${slideIndex * (100 / slidesToShow)}%)`;
-// }
-
-// function plusSlides(n) {
-//     showSlides(slideIndex + n);
-// }
-
-// window.addEventListener('resize', () => {
-//     const newSlidesToShow = window.innerWidth > 600 ? 3 : 1;
-//     if (newSlidesToShow !== slidesToShow) {
-//         location.reload(); // Reload to reset the layout
-//     }
-// });
-
-// showSlides(slideIndex);
+window.addEventListener('resize', () => {
+  showSlides(slideIndex);
+});
